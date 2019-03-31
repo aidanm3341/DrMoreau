5cda2f8ed6c8e506fcbb044fb408cad1ef0e48a7
Jimmy Tam <tamk7@mcmaster.ca>
2019-03-31 15:07:55 -0400

    started confirmation panel

---
+++ src/main/java/screens/ConfirmationPanel.java
@@ -0,0 +1,16 @@
+package screens;
+
+import main.Main;
+import org.newdawn.slick.*;
+import org.newdawn.slick.gui.AbstractComponent;
+import org.newdawn.slick.gui.ComponentListener;
+import org.newdawn.slick.state.StateBasedGame;
+import util.Button;
+import util.ResourceLoader;
+
+public class ConfirmationPanel extends Screen implements ComponentListener {
+
+    private Button ConfirmButton;
+    private Button CancelButton;
+
+}
